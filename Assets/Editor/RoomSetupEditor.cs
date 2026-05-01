9bf6be63bdf8f654510938f6586b6fa2cd8bac7b
Coby Davis <cobylax17@gmail.com>
2026-05-01 08:06:44 -0600

    editor script for building rooms

---
+++ Assets/Editor/RoomSetupEditor.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoomSetup))]
+public class RoomSetupEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        RoomSetup roomSetup = (RoomSetup)target;
+
+        if (GUILayout.Button("Build Room"))
+        {
+            Undo.RegisterCompleteObjectUndo(roomSetup, "Build Room");
+            Undo.RegisterCompleteObjectUndo(roomSetup.gameObject, "Build Room");
+            BoxCollider2D collider = roomSetup.GetComponent<BoxCollider2D>();
+            Undo.RegisterCompleteObjectUndo(collider, "Build Room");
+
+            RoomSetup(roomSetup, collider);
+            ClearWalls(roomSetup);
+            BuildWalls(roomSetup, 0, 1);
+            BuildWalls(roomSetup, 0, 2);
+            BuildWalls(roomSetup, 1, 3);
+            BuildWalls(roomSetup, 2, 3);
+        }
+
+        DrawDefaultInspector();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void OnSceneGUI()
+    {
+        RoomSetup roomSetup = (RoomSetup)target;
+
+        Handles.color = Color.cyan;
+
+        for(int i = 0; i < roomSetup.handles.Count; i++)
+        {
+            Vector3 handle = roomSetup.handles[i];
+            EditorGUI.BeginChangeCheck();
+            Vector3 newPos = Handles.FreeMoveHandle(handle, 0.5f, Vector3.one * 5f, Handles.SphereHandleCap);
+            float xPos = Mathf.Round((newPos.x - roomSetup.gridCenter.x) / roomSetup.nodeSize) * roomSetup.nodeSize + roomSetup.gridCenter.x + roomSetup.nodeSize / 2;
+            float yPos = Mathf.Round((newPos.y - roomSetup.gridCenter.y) / roomSetup.nodeSize) * roomSetup.nodeSize + roomSetup.gridCenter.y + roomSetup.nodeSize / 2;
+            newPos = new Vector3(xPos, yPos, 0);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RegisterCompleteObjectUndo(roomSetup, "Move Room Setup Handle");
+                roomSetup.handles[i] = newPos;
+                Vector3 xLockedPos = roomSetup.handles[roomSetup.xLocks[i]];
+                roomSetup.handles[roomSetup.xLocks[i]] = new Vector3(newPos.x, xLockedPos.y, 0);
+                Vector3 yLockedPos = roomSetup.handles[roomSetup.yLocks[i]];
+                roomSetup.handles[roomSetup.yLocks[i]] = new Vector3(yLockedPos.x, newPos.y, 0);
+                EditorUtility.SetDirty(roomSetup);
+            }
+        }
+    }
+
+    void RoomSetup(RoomSetup roomSetup, BoxCollider2D collider)
+    {
+        float xPos = (roomSetup.handles[0].x + roomSetup.handles[1].x) / 2f;
+        float yPos = (roomSetup.handles[0].y + roomSetup.handles[2].y) / 2f;
+        roomSetup.transform.position = new Vector3(xPos, yPos, 0);
+        float xWidth = Vector3.Distance(roomSetup.handles[0], roomSetup.handles[1]) - roomSetup.wallWidth;
+        float yWidth = Vector3.Distance(roomSetup.handles[0], roomSetup.handles[2]) - roomSetup.wallWidth;
+        collider.size = new Vector2(xWidth, yWidth);
+    }
+
+    void ClearWalls(RoomSetup roomSetup)
+    {
+        foreach(GameObject wall in roomSetup.wallsList)
+        {
+            Undo.DestroyObjectImmediate(wall);
+        }
+        roomSetup.wallsList.Clear();
+    }
+
+    void BuildWalls(RoomSetup roomSetup, int index1, int index2)
+    {
+        Vector3 point1 = roomSetup.handles[index1];
+        Vector3 point2 = roomSetup.handles[index2];
+        float distance = Vector3.Distance(point1, point2);
+        Vector3 direction = Vector3.Normalize(point2 - point1);
+        Vector3 midPoint = (point1 + point2) / 2;
+
+        GameObject newWall = (GameObject)PrefabUtility.InstantiatePrefab(roomSetup.wallPrefab);
+        Undo.RegisterCreatedObjectUndo(newWall, "Create wall");
+        newWall.transform.SetParent(roomSetup.wallsParent.transform);
+        newWall.transform.position = midPoint;
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            newWall.transform.localScale = new Vector3(distance + roomSetup.wallWidth, roomSetup.wallWidth, 1);
+        }
+        else
+        {
+            newWall.transform.localScale = new Vector3(roomSetup.wallWidth, distance + roomSetup.wallWidth, 1);
+        }
+        roomSetup.wallsList.Add(newWall);
+    }
+}
