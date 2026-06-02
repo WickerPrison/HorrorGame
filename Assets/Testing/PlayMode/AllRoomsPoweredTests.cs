c6dca35f2625bc792c96c19cd361ad543073a799
Coby Davis <cobylax17@gmail.com>
2026-06-02 07:25:24 -0600

    add test to ensure all rooms are powered

---
+++ Assets/Testing/PlayMode/AllRoomsPoweredTests.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class AllRoomsPoweredTests
+{
+    IEnumerator AllRoomsHavePower(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        yield return null;
+        Terminal[] terminals = Object.FindObjectsByType<Terminal>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        Room[] rooms = Object.FindObjectsByType<Room>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        yield return null;
+        bool allRoomsHavePower = true;
+        foreach(Room room in rooms)
+        {
+            bool roomHasPower = false;
+            if(room.portal != null && room.portal.providesPower)
+            {
+                roomHasPower = true;
+                continue;
+            }
+
+            bool poweredByTerminal = false;
+            foreach(Terminal terminal in terminals)
+            {
+                if (terminal.roomsToPower.Contains(room))
+                {
+                    poweredByTerminal = true;
+                    break;
+                }
+            }
+            if (poweredByTerminal)
+            {
+                roomHasPower = true;
+                continue;
+            }
+
+            if (!roomHasPower)
+            {
+                Debug.Log($"Room {room.name} does not have power");
+                allRoomsHavePower = false;
+            }
+        }
+        Assert.IsTrue(allRoomsHavePower);
+    }
+
+    [UnityTest]
+    public IEnumerator Level1()
+    {
+        yield return AllRoomsHavePower("Level1");
+    }
+}
