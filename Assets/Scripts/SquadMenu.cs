dd4956457623a1af531135169d54e0a6b2b82fbd
Coby Davis <cobylax17@gmail.com>
2026-06-15 17:52:25 -0600

    move squad members around

--- Assets/Scripts/SquadMenu.cs
+++ Assets/Scripts/SquadMenu.cs
@@ -8,9 +8,39 @@ public class SquadMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < campaignData.squad.Length; i++)
+        UpdateSquadUi();
+    }
+
+    void UpdateSquadUi()
+    {
+        for (int i = 0; i < campaignData.squad.Length; i++)
         {
+            if (campaignData.squad[i] != null)
+            {
+                campaignData.squad[i].index = i;
+            }
             squadUnits[i].SetUnitData(campaignData.squad[i]);
         }
+    }
+
+    public void RemoveFromSquad(int index)
+    {
+        campaignData.squad[index] = null;
+        UpdateSquadUi();
+        CampaignEvents.i.UpdateSquad();
+    }
+
+    public void MoveUnitUp(int index)
+    {
+        if (index == 0) return;
+        (campaignData.squad[index], campaignData.squad[index - 1]) = (campaignData.squad[index - 1], campaignData.squad[index]);
+        UpdateSquadUi();
+    }
+
+    public void MoveUnitDown(int index)
+    {
+        if (index == 3) return;
+        (campaignData.squad[index], campaignData.squad[index + 1]) = (campaignData.squad[index + 1], campaignData.squad[index]);
+        UpdateSquadUi();
     }
 }
--- Assets/Scripts/SquadUnit.cs
+++ Assets/Scripts/SquadUnit.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class SquadUnit : MonoBehaviour
 {
     UnitStatUi statsUi;
+    [SerializeField] MenuButton remove;
+    [SerializeField] MenuButton up;
+    [SerializeField] MenuButton down;
 
     private void Awake()
     {
@@ -11,6 +14,15 @@ public class SquadUnit : MonoBehaviour
 
     public void SetUnitData(PlayerUnitData unitData)
     {
+        bool hasUnit = unitData != null;
+        remove.gameObject.SetActive(hasUnit);
+        up.gameObject.SetActive(hasUnit);
+        down.gameObject.SetActive(hasUnit);
+        if(hasUnit)
+        {
+            up.interactable = unitData.index != 0;
+            down.interactable = unitData.index != 3;
+        }
         statsUi.SetUnit(unitData);
     }
 }
